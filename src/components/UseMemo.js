4d900ee27086b51d15c0f511ee5a1fde34afbcd4
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-11-02 19:43:38 +0530

    Create UseMemo.js

---
+++ src/components/UseMemo.js
@@ -0,0 +1,17 @@
+
+import React, { useMemo } from "react";
+
+const UseMemo = ({ counter }) => {
+  const factorial = useMemo(() => {
+    const computeFactorial = (n) => (n <= 1 ? 1 : n * computeFactorial(n - 1));
+    return computeFactorial(counter);
+  }, [counter]);
+
+  return (
+    <div>
+      <h3>Factorial of counter: {factorial}</h3>
+    </div>
+  );
+};
+
+export default UseMemo;
